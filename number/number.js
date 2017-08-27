e3521edfb91256b02fdb8c5c403ed90b9d30275f
aangel105 <31191140+aangel105@users.noreply.github.com>
2017-08-27 15:21:04 -0400

    Create number.js

---
+++ number/number.js
@@ -0,0 +1,19 @@
+var x= prompt('enter a mathematical operator follow by two number')
+
+var readline = require('readline')
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+
+rl.on('line', function(input) {
+   
+    var num = Number(input)
+   
+    if (isNaN(num)) {
+      console.log('that is not a valid number')
+    } else {
+      console.log('your input was the number: ' + num)
+    }
+  })
